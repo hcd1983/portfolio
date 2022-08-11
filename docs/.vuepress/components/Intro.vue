9f4fab1ba210cf981ffc118502130d92b62b4a7d
Dean Huang <hcd@mojopot.com>
2022-08-11 17:12:34 +0800

    cu

--- docs/.vuepress/components/Intro.vue
+++ docs/.vuepress/components/Intro.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="flex h-screen w-screen max-w-full flex-col justify-center items-center">
-  <h1 class="font-cursive text-9xl">HCD</h1>
+  <h1 class="font-cursive text-9xl"><span>HCD</span></h1>
   <hr class="border-[3px] border-gray-700 w-[90%] my-3 max-w-[500px]" />
   <h1 class="font-cursive text-8xl">design</h1>
 </div>
@@ -13,8 +13,28 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  width: fit-content;
+}
+h1 span{
+  display: flex;
+  justify-content: center;
+  opacity: 0;
+  width: 0;
+  margin: auto;
+  transition: all .5s ease-in-out;
+  overflow: hidden;
+  transform: scale(.7);
+}
+
+h1.active span{
+  opacity: 1;
+  width: 100%;
+  transform: scale(1);
+}
+
 hr {
   transform: skewX(30deg);
-  border-image: url("https://png.pngtree.com/png-clipart/20170312/ourmid/pngtree-vector-painted-black-and-white-brush-art-white-brush-effect-png-image_3041429.png") 100 round;
+  /*border-image: url("https://png.pngtree.com/png-clipart/20170312/ourmid/pngtree-vector-painted-black-and-white-brush-art-white-brush-effect-png-image_3041429.png") 100 round;*/
 }
 </style>
--- docs/.vuepress/components/Works.vue
+++ docs/.vuepress/components/Works.vue
@@ -3,7 +3,7 @@
   <div class="flex flex-col h-screen">
     <h3 class="text-6xl font-cursive pt-3">Works</h3>
     <div ref="target" class="flex w-fit flex-1 pt-3">
-      <div v-for="i in 20" class="h-full w-[50vw] bg-blue-300 even:bg-red-300 flex-shrink-0 flex items-center justify-center">
+      <div v-for="i in 20" class="h-full w-[1800px] bg-blue-300 even:bg-red-300 flex-shrink-0 flex items-center justify-center">
         <span class="text-3xl">{{ i }}</span>
       </div>
     </div>
