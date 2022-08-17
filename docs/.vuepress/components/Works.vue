6ef700dd6d9ce09b1065bfa16ce6b9e666fd34ef
Dean Huang <hcd@mojopot.com>
2022-08-18 00:22:57 +0800

    better scroll animation

--- docs/.vuepress/components/Works.vue
+++ docs/.vuepress/components/Works.vue
@@ -58,7 +58,7 @@ export default {
     })
   },
   updated() {
-    // this.resetTrigger()
+    this.resetTrigger()
   },
   beforeUnmount() {
     this.disableTrigger()
@@ -98,9 +98,9 @@ export default {
           ...triggerOptions,
           snap: {
             snapTo: "labels", // snap to the closest label in the timeline
-            duration: {min: 0.1, max: 1}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-            delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-            // ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
+            duration: {min: .2, max: .5}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+            delay: 0.1, // wait 0.2 seconds from the last scroll event before doing the snapping
+            ease: "power1.inOut"
           }
         }
       });
