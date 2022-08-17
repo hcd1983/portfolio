3e9c30bdbaffe3809a044651544a739d3abd12b4
Dean Huang <hcd@mojopot.com>
2022-08-18 07:44:16 +0800

    Updates

--- assets/index.html.15a104e4.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as r,c as l,a as e,r as o}from"./app.fc67fea7.js";const a={};function i(m,p){const n=o("about"),t=o("Skills"),c=o("timeline"),_=o("works");return r(),l("div",null,[e(n),e(t),e(c),e(_)])}var f=s(a,[["render",i],["__file","index.html.vue"]]);export{f as default};
